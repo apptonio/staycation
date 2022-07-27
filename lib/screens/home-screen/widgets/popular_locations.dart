a26903cf06a98271437ecd11371a99100a8b6a9c
Antonio725 <ab7719@g.rit.edu>
2022-07-28 00:39:00 +0200

    add stateless to widgets

---
+++ lib/screens/home-screen/widgets/popular_locations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PopularLocations extends StatelessWidget {
+  const PopularLocations({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return(const Text("Popular Locations"));
+  }
+}
---
+++ lib/screens/home-screen/widgets/section_title_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return(const Text("Title"));
+  }
+}
