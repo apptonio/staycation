2023f9c7acaeb1eea95b3ce92f6751bb3ac1b786
Antonio725 <ab7719@g.rit.edu>
2022-07-28 09:57:34 +0200

    add icon assets

--- lib/assets.dart
+++ lib/assets.dart
@@ -4,7 +4,27 @@ class Assets {
 }
 
 class _Icons {
+  String add = 'assets/icons/add.svg';
+  String attachment = 'assets/icons/attachment.svg';
+  String back = 'assets/icons/back.svg';
+  String calendar = 'assets/icons/calendar.svg';
+  String close = 'assets/icons/close.svg';
+  String delete = 'assets/icons/delete.svg';
   String devcademy = 'assets/icons/devcademy.png';
+  String down = 'assets/icons/down.svg';
+  String edit = 'assets/icons/edit.svg';
+  String filter = 'assets/icons/filter.svg';
+  String home = 'assets/icons/home.svg';
+  String hotel = 'assets/icons/hotel.svg';
+  String list = 'assets/icons/list.svg';
+  String logout = 'assets/icons/logout.svg';
+  String menu = 'assets/icons/menu.svg';
+  String more = 'assets/icons/more.svg';
+  String person = 'assets/icons/person.svg';
+  String places = 'assets/icons/places.svg';
+  String remove = 'assets/icons/remove.svg';
+  String search = 'assets/icons/search.svg';
+  String upload = 'assets/icons/upload.svg';
 }
 
 class _Images {
