916613123014f28e3a6ee4a24dae273b911f3b3a
Antonio725 <ab7719@g.rit.edu>
2022-07-28 02:27:36 +0200

    add home screen widget tree

--- lib/screens/home-screen/home_screen.dart
+++ lib/screens/home-screen/home_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
+import './widgets/popular_locations.dart';
+import './widgets/homes_guests_love.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text('welcome to devcademy'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Staycation'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more))
+        ],
+      ),
+      body: SafeArea(
+          child:
+              Column(children: const [PopularLocations(), HomesGuestsLove()])),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.teal),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'My Bookings',
+              backgroundColor: Colors.teal),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.place),
+              label: 'My Places',
+              backgroundColor: Colors.teal)
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.teal,
       ),
     );
   }
