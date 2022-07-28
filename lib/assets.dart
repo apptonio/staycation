6ebe7ddd99fee578a6c7ee77005099a5378dcbd1
Antonio725 <ab7719@g.rit.edu>
2022-07-28 09:16:32 +0200

    Add image assets

--- lib/assets.dart
+++ lib/assets.dart
@@ -7,4 +7,7 @@ class _Icons {
   String devcademy = 'assets/icons/devcademy.png';
 }
 
-class _Images {}
+class _Images {
+  String tokyo = 'assets/images/Tokyo.png';
+  String sugar = 'assets/images/Sugar.png';
+}
