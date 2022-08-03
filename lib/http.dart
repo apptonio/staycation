4e9209990a8c3c5c7e5de727b8b3991896f893f5
Antonio725 <ab7719@g.rit.edu>
2022-08-03 15:39:28 +0200

    add api calls

--- lib/http.dart
+++ lib/http.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 
+import 'models/location.dart';
+
 class HTTPInterceptor extends InterceptorsWrapper {
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
@@ -13,9 +15,7 @@ class HTTP {
   late Dio client;
   HTTP() {
     client = Dio(BaseOptions(
-      // ignore: todo
-      //TODO: replace with real backend url
-      baseUrl: 'backendUrl',
+      baseUrl: 'https://62a17fb4cd2e8da9b0f28a7a.mockapi.io/api/staycation',
       receiveDataWhenStatusError: true,
       contentType: 'application/json',
       validateStatus: (status) => status! < 400,
@@ -23,6 +23,42 @@ class HTTP {
     client.interceptors.add(
       HTTPInterceptor(),
     );
+  }
+
+  Future<List<Location>> getPopularLocations() async {
+    Response response = await client.get(
+      'locations',
+    );
+    return response.data
+        .map<Location>((json) => Location.fromJson(json))
+        .toList();
+  }
+
+  Future<List<Location>> getHomesGuestsLove() async {
+    Response response = await client.get(
+      'homes',
+    );
+    return response.data
+        .map<Location>((json) => Location.fromJson(json))
+        .toList();
+  }
+
+  Future<List<Location>> getAllLocations() async {
+    Response response = await client.get(
+      'locations-all',
+    );
+    return response.data
+        .map<Location>((json) => Location.fromJson(json))
+        .toList();
+  }
+
+  Future<List<Location>> getAllHomes() async {
+    Response response = await client.get(
+      'homes-all',
+    );
+    return response.data
+        .map<Location>((json) => Location.fromJson(json))
+        .toList();
   }
 }
 
