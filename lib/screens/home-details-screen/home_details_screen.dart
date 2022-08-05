1c326d3c602f3172dd2062fc5d4f641071e8d7c6
Antonio725 <ab7719@g.rit.edu>
2022-08-05 02:25:38 +0200

    add details sheet

--- lib/screens/home-details-screen/home_details_screen.dart
+++ lib/screens/home-details-screen/home_details_screen.dart
@@ -1,10 +1,64 @@
+import 'package:devcademy_flutter/screens/home-details-screen/widgets/details_sheet.dart';
+import 'package:devcademy_flutter/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomeDetailsScreen extends StatelessWidget {
-  const HomeDetailsScreen({Key? key}) : super(key: key);
+  const HomeDetailsScreen(
+      {Key? key,
+      required this.id,
+      required this.imageUrl,
+      required this.title,
+      required this.shortDescription,
+      required this.longDescription,
+      required this.location,
+      required this.postalCode,
+      required this.price,
+      required this.categorization,
+      required this.capacity,
+      required this.accommodationType,
+      required this.freeCancelation})
+      : super(key: key);
+
+  final String id;
+  final String imageUrl;
+  final String title;
+  final String shortDescription;
+  final String longDescription;
+  final String location;
+  final String postalCode;
+  final int price;
+  final int categorization;
+  final int capacity;
+  final String accommodationType;
+  final bool freeCancelation;
 
   @override
   Widget build(BuildContext context) {
-    return (Container());
+    return (Scaffold(
+      body: Column(
+        children: [
+          Image.network(
+            imageUrl,
+            width: double.infinity,
+          ),
+          
+          DetailsSheet(id: id, title: title, shortDescription: shortDescription, longDescription: longDescription, location: location, postalCode: postalCode, price: price, categorization: categorization, capacity: capacity, accommodationType: accommodationType, freeCancelation: freeCancelation),
+
+          SafeArea(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SizedBox(
+                    height: 42.0,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("BOOK YOUR STAY",
+                          style: textTheme.button
+                              ?.merge(TextStyle(color: ThemeColors.white))),
+                    ),
+                  ))),
+        ],
+      ),
+    ));
   }
 }
